1c44c34fd3a6452569c8f56a89673dd6440c907e
Kristaps <azorien@gmail.com>
2021-08-24 00:24:30 +0300

    Add Prettier config and format files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <div id="nav"><router-link to="/">Home</router-link></div>
-        <router-view />
-    </div>
+  <div>
+    <div id="nav"><router-link to="/">Home</router-link></div>
+    <router-view />
+  </div>
 </template>
