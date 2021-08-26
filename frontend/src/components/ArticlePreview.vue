fcbf9326d774c5c68c305f7e4d172185c2814551
Kristaps <azorien@gmail.com>
2021-08-26 23:13:42 +0300

    Add ArticlePreview component that shows image, title and description

---
+++ frontend/src/components/ArticlePreview.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <div
+      class="article-image"
+      :style="{ backgroundImage: `url(${img})` }"
+    ></div>
+    <div class="article-text">
+      <h3>{{ title }}</h3>
+      <p>{{ text }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticlePreviewSmall',
+  props: {
+    title: String,
+    text: String,
+    img: String,
+  },
+}
+</script>
+
+<style scoped>
+.article-image {
+  width: 50%;
+  height: 18rem;
+  color: #fff;
+
+  display: inline-block;
+
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+}
+
+.article-text {
+  width: 50%;
+  color: #000;
+
+  display: inline-block;
+  justify-content: center;
+  /* debug */
+  border: 1px solid darkorange;
+  box-sizing: border-box;
+}
+
+.container:hover {
+  cursor: pointer;
+  opacity: 0.6;
+}
+
+.container {
+  width: 100%;
+  height: 18rem;
+  margin: 5px;
+
+  display: flex;
+
+  /* debug */
+  border: 1px solid fuchsia;
+  box-sizing: border-box;
+}
+</style>
--- frontend/src/views/Posts.vue
+++ frontend/src/views/Posts.vue
@@ -1,9 +1,44 @@
 <template>
-  <div></div>
+  <div>
+    <ArticlePreview
+      v-for="(article, index) in articles"
+      :key="`article-${index}`"
+      :title="article.title"
+      :text="article.text"
+      img="https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg"
+    />
+  </div>
 </template>
 
 <script>
+import ArticlePreview from '@/components/ArticlePreview'
+
 export default {
   name: 'Posts',
+  components: {
+    ArticlePreview,
+  },
+  data() {
+    return {
+      articles: [
+        {
+          title: 'Article title',
+          text: 'Some description goes here for this article',
+        },
+        {
+          title: 'Article title',
+          text: 'Some description goes here for this article',
+        },
+        {
+          title: 'Article title',
+          text: 'Some description goes here for this article',
+        },
+        {
+          title: 'Article title',
+          text: 'Some description goes here for this article',
+        },
+      ],
+    }
+  },
 }
 </script>
