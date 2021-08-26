897ad2cb56e735d7d3edbf3cd0d036522b6ae7d9
Kristaps <azorien@gmail.com>
2021-08-26 16:39:38 +0300

    Add ArticlePreviewSmall component for article preview cards

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <ArticlePreviewSmall
+      v-for="(article, index) in articles"
+      :key="`article-${index}`"
+      :title="article.title"
+    />
+  </div>
 </template>
 
 <script>
+import ArticlePreviewSmall from '@/components/ArticlePreviewSmall'
+
 export default {
   name: 'Home',
+  components: {
+    ArticlePreviewSmall,
+  },
+  data() {
+    return {
+      articles: [
+        { title: 'Title 1' },
+        { title: 'Title 2' },
+        { title: 'Title 3' },
+      ],
+    }
+  },
 }
 </script>
