23df32c0d428c15ad36fa4cb377919f6a2f621f6
hedgeinthehog <37834796+hedgeinthehog@users.noreply.github.com>
2021-08-27 05:26:52 -0700

    fixes trailing comma issue in posts migrations

--- backend/storages/DbStorage/migrations/1629737809810_initiate_posts_table.sql
+++ backend/storages/DbStorage/migrations/1629737809810_initiate_posts_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS posts (
     `title` varchar(50) not null,
     `text` varchar(4000) not null,
     `description` varchar(280) not null,
-    `image_url` varchar(255) not null,
+    `image_url` varchar(255) not null
     -- `user_id` int,
     -- FOREIGN KEY (user_id) REFERENCES users(id)
 );
