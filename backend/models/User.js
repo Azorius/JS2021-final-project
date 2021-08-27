f72eb110ac320448925229fc14ce60315bae677c
hedgethehog <micko.eleonora@gmail.com>
2021-08-27 14:45:55 +0300

    creates user model

---
+++ backend/models/User.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcryptjs');
+
+class User {
+  constructor({ name, email }) {
+    this._data = {
+      name,
+      email,
+    };
+  }
+
+  getData() {
+    return Object.assign({}, this._data);
+  }
+
+  setId(id) {
+    this._data.id = id;
+  }
+
+  setPassword(password) {
+    this._data.password = bcrypt.hashSync(password, 10);
+  }
+
+  comparePassword(password) {
+    return bcrypt.compareSync(password, this._data.password)
+  }
+
+  getDataForStorage() {
+    const dataCopy = Object.assign({}, this._data);
+    return dataCopy;
+  }
+}
+
+module.exports = User;
