61737519a37164b875ea0e685cacd8ecfc281450
Kristaps <azorien@gmail.com>
2021-08-27 14:21:59 +0300

    Change data source to vuex state in Home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <ArticlePreviewSmall
-      v-for="(article, index) in articles"
+      v-for="(latestArticles, index) in articles"
       :key="`article-${index}`"
       :title="article.title"
       img="https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg"
@@ -11,20 +11,15 @@
 
 <script>
 import ArticlePreviewSmall from '@/components/ArticlePreviewSmall'
+import { mapGetters } from 'vuex'
 
 export default {
   name: 'Home',
   components: {
     ArticlePreviewSmall,
   },
-  data() {
-    return {
-      articles: [
-        { title: 'Title 1' },
-        { title: 'Title 2' },
-        { title: 'Title 3' },
-      ],
-    }
+  computed: {
+    ...mapGetters(['latestArticles']),
   },
 }
 </script>
