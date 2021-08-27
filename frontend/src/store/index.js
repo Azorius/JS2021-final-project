1013c14db4e18b27a96fee4f7aec50bca3520284
Kristaps <azorien@gmail.com>
2021-08-27 17:27:10 +0300

    Remove dummy data and use server data

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,40 +4,7 @@ const axios = require('axios')
 export default createStore({
   strict: true,
   state: {
-    articles: [
-      {
-        id: 1,
-        title: 'Title 1',
-        description: 'Test description 1',
-        text: 'Test article text 1',
-        img:
-          'https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg',
-      },
-      {
-        id: 1,
-        title: 'Title 2',
-        description: 'Test description 2',
-        text: 'Test article text 2',
-        img:
-          'https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg',
-      },
-      {
-        id: 1,
-        title: 'Title 3',
-        description: 'Test description 3',
-        text: 'Test article text 3',
-        img:
-          'https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg',
-      },
-      {
-        id: 1,
-        title: 'Title 4',
-        description: 'Test description 4',
-        text: 'Test article text 4',
-        img:
-          'https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg',
-      },
-    ],
+    articles: [],
   },
   getters: {
     latestArticles(state) {
@@ -53,12 +20,13 @@ export default createStore({
     },
   },
   actions: {
-    requestArticles() {
-      console.log('request articles')
+    requestArticles(context) {
       axios
         .get('http://localhost:3000/api/posts')
         .then(response => {
-          console.log(response.data.data.posts)
+          context.commit('updateArticles', {
+            articles: response.data.data.posts,
+          })
         })
         .catch(error => {
           console.log(error)
