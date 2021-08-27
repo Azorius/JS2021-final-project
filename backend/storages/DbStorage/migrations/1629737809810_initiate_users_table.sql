2044bacb57bdb923b782c69107aa0c988f1d0e3b
hedgethehog <micko.eleonora@gmail.com>
2021-08-28 01:55:56 +0300

    updates users table structure

--- backend/storages/DbStorage/migrations/1629737809810_initiate_users_table.sql
+++ backend/storages/DbStorage/migrations/1629737809810_initiate_users_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     `id` int PRIMARY KEY auto_increment,
-    `name` varchar(255) not null,
+    `name` varchar(30) not null,
     `email` varchar(255) not null,
-    `password` varchar(255) not null
+    `password` varchar(255) not null,
+    `token` varchar(255) default null
 );
