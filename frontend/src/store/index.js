4e14aa1e9392c38ed8b46ea7e5098766534a7bdb
Kristaps <azorien@gmail.com>
2021-08-27 14:31:05 +0300

    Move img to dummy data

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -9,24 +9,32 @@ export default createStore({
         title: 'Title 1',
         description: 'Test description 1',
         text: 'Test article text 1',
+        img:
+          'https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg',
       },
       {
         id: 1,
         title: 'Title 2',
         description: 'Test description 2',
         text: 'Test article text 2',
+        img:
+          'https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg',
       },
       {
         id: 1,
         title: 'Title 3',
         description: 'Test description 3',
         text: 'Test article text 3',
+        img:
+          'https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg',
       },
       {
         id: 1,
         title: 'Title 4',
         description: 'Test description 4',
         text: 'Test article text 4',
+        img:
+          'https://petkeen.com/wp-content/uploads/2020/06/shutterstock_776419591-e1593876689446.jpg',
       },
     ],
   },
