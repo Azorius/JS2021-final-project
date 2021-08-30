0781f3959c7bb4d632db91b4254305df43214f87
Kristaps <azorien@gmail.com>
2021-08-30 17:45:48 +0300

    Style title, description and author block

--- frontend/src/components/ArticlePreview.vue
+++ frontend/src/components/ArticlePreview.vue
@@ -4,9 +4,15 @@
       class="article-image"
       :style="{ backgroundImage: `url(${img})` }"
     ></div>
-    <div class="article-text">
-      <h3>{{ title }}</h3>
-      <p>{{ text }}</p>
+    <div class="article-contents-wrapper">
+      <div class="contents">
+        <h2>{{ title }}</h2>
+        <p>{{ text }}</p>
+        <div class="author-block">
+          <img src="@/assets/placeholder_icon.png" />
+          <p>Username Placeholder</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,18 +30,40 @@ export default {
 
 <style scoped>
 .article-image {
-  flex: 1;
+  width: 50%;
 
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
 }
 
-.article-text {
-  flex: 1;
-  color: #000;
+.article-contents-wrapper {
+  width: 50%;
+}
 
-  justify-content: center;
+.contents {
+  height: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+}
+
+.contents > p {
+  flex: 1;
+}
+
+.author-block {
+  display: flex;
+  flex-direction: row;
+}
+.author-block > img {
+  width: 3rem;
+  height: 3rem;
+}
+.author-block > p {
+  margin: auto;
+  margin-left: 20px;
 }
 
 .container:hover {
@@ -59,12 +87,12 @@ export default {
 
   .article-image {
     width: 100%;
-    flex: 1;
+    height: 30%;
   }
 
-  .article-text {
+  .article-contents-wrapper {
     width: 100%;
-    flex: 1.5;
+    height: 70%;
   }
 }
 </style>
