4b3af2599d427ebf60e90866e06f06639b972ab0
Kristaps <azorien@gmail.com>
2021-09-01 00:53:59 +0300

    Add content to single article page

--- frontend/src/views/SinglePost.vue
+++ frontend/src/views/SinglePost.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="view">
-    <div class="image-header">
-      <h2>Title</h2>
+    <div
+      class="article-image"
+      :style="{
+        backgroundImage: `url(http://localhost:3000/pictures/${article.imgName})`,
+      }"
+    >
+      <h2>{{ article.title }}</h2>
     </div>
-    <p>Description</p>
-    <p>Text</p>
+    <p>{{ article.description }}</p>
+    <p>{{ article.text }}</p>
   </div>
 </template>
 <script>
 export default {
   name: 'SinglePost',
+  data() {
+    return {
+      article: {},
+    }
+  },
+  mounted() {
+    this.article = this.$store.getters.articles.find(
+      article => article.id == this.$route.params.id
+    )
+  },
 }
 </script>
 
@@ -17,5 +32,14 @@ export default {
 .view {
   display: flex;
   flex-direction: column;
+  width: 100%;
+}
+
+.article-image {
+  width: 100%;
+
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
 }
 </style>
