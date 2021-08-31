e328572cbd8505d0b445a32fe573909ef103d24a
hedgethehog <micko.eleonora@gmail.com>
2021-08-31 15:05:48 +0300

    adds trycatch blocks in storage functions

--- backend/storages/DbStorage/index.js
+++ backend/storages/DbStorage/index.js
@@ -23,97 +23,123 @@ class DbStorage {
     this._table = tableName;
   }
   async getAll(query) {
-    let [rows] = await pool.promise().execute(foreignKeysStatement);
-    [rows] = await pool.promise().execute(`
-      SELECT * 
-      FROM ${this._table} 
-      JOIN ${rows[0].foreign_table} 
-      ON ${this._table}.${rows[0].column_name} = ${rows[0].foreign_table}.${rows[0].foreign_column}
-      ${query
-        ? `${query.startWith ? `WHERE id_${this._table} >= ${query.startWith}` : ''}
-          ${query.limit ? `ORDER BY id_${this._table} ASC LIMIT ${query.limit}` : ''}` 
-        : ''}
-    `);
-    return rows;
+    try {
+      let [rows] = await pool.promise().execute(foreignKeysStatement);
+      [rows] = await pool.promise().execute(`
+        SELECT * 
+        FROM ${this._table} 
+        JOIN ${rows[0].foreign_table} 
+        ON ${this._table}.${rows[0].column_name} = ${rows[0].foreign_table}.${rows[0].foreign_column}
+        ${query
+          ? `${query.startWith ? `WHERE id_${this._table} >= ${query.startWith}` : ''}
+            ${query.limit ? `ORDER BY id_${this._table} ASC LIMIT ${query.limit}` : ''}` 
+          : ''}
+      `);
+      return rows;
+    } catch (e) {
+      throw new Error(e);
+    }
   }
   async getFiltered(filter, query) {
-    const field = Object.entries(filter)[0];
-    let [rows] = await pool.promise().execute(foreignKeysStatement);
-    [rows] = await pool.promise().execute(
-      `
-      SELECT * 
-      FROM ${this._table} 
-      JOIN ${rows[0].foreign_table} 
-      ON ${this._table}.${rows[0].column_name} = ${rows[0].foreign_table}.${rows[0].foreign_column} 
-      WHERE ${field[0]} = ?
-      ${query
-        ? `${query.startWith ? `AND id_${this._table} >= ${query.startWith}` : ''}
-          ${query.limit ? `ORDER BY id_${this._table} ASC LIMIT ${query.limit}` : ''}` 
-        : ''}`,
-      [field[1]]
-    );
-    return rows;
+    try {
+      const field = Object.entries(filter)[0];
+      let [rows] = await pool.promise().execute(foreignKeysStatement);
+      [rows] = await pool.promise().execute(
+        `
+        SELECT * 
+        FROM ${this._table} 
+        JOIN ${rows[0].foreign_table} 
+        ON ${this._table}.${rows[0].column_name} = ${rows[0].foreign_table}.${rows[0].foreign_column} 
+        WHERE ${field[0]} = ?
+        ${query
+          ? `${query.startWith ? `AND id_${this._table} >= ${query.startWith}` : ''}
+            ${query.limit ? `ORDER BY id_${this._table} ASC LIMIT ${query.limit}` : ''}` 
+          : ''}`,
+        [field[1]]
+      );
+      return rows;      
+    } catch (e) {
+      throw new Error(e);      
+    }
   }
   async getById(id) {
-    let [rows] = await pool.promise().execute(foreignKeysStatement);
-    [rows] = await pool.promise().execute(
-      `
-      SELECT * 
-      FROM ${this._table} 
-      JOIN ${rows[0].foreign_table} 
-      ON ${this._table}.${rows[0].column_name} = ${rows[0].foreign_table}.${rows[0].foreign_column}
-      WHERE id_${this._table} = ?`,
-      [id]
-    );
-    return rows[0];
+    try {
+      let [rows] = await pool.promise().execute(foreignKeysStatement);
+      [rows] = await pool.promise().execute(
+        `
+        SELECT * 
+        FROM ${this._table} 
+        JOIN ${rows[0].foreign_table} 
+        ON ${this._table}.${rows[0].column_name} = ${rows[0].foreign_table}.${rows[0].foreign_column}
+        WHERE id_${this._table} = ?`,
+        [id]
+      );
+      return rows[0];
+    } catch (e) {
+      throw new Error(e);      
+    }
   }
   async getOne(filter) {
-    const field = Object.entries(filter)[0];
-    const [rows] = await pool
-      .promise()
-      .execute(
-        `SELECT * FROM ${this._table} WHERE ${field[0]} = '${field[1]}'`
-      );
-    return rows[0];
+    try {
+      const field = Object.entries(filter)[0];
+      const [rows] = await pool
+        .promise()
+        .execute(
+          `SELECT * FROM ${this._table} WHERE ${field[0]} = '${field[1]}'`
+        );
+      return rows[0];
+    } catch (e) {
+      throw new Error(e);      
+    }
   }
   async create(data) {
-    const fields = Object.keys(data);
-    const values = Object.values(data);
-
-    let dataLength = values.length - 1;
-    let valuesPlaceholder = '?';
-    while (dataLength) {
-      valuesPlaceholder += ', ?';
-      dataLength -= 1;
+    try {
+      const fields = Object.keys(data);
+      const values = Object.values(data);
+  
+      let dataLength = values.length - 1;
+      let valuesPlaceholder = '?';
+      while (dataLength) {
+        valuesPlaceholder += ', ?';
+        dataLength -= 1;
+      }
+  
+      const [meta] = await pool
+        .promise()
+        .execute(
+          `INSERT INTO ${this._table} (${fields}) VALUES (${valuesPlaceholder})`,
+          values
+        );
+  
+      return Object.assign({}, data, { [`id_${this._table}`]: meta.insertId });
+    } catch (e) {
+      throw new Error(e);      
     }
-
-    const [meta] = await pool
-      .promise()
-      .execute(
-        `INSERT INTO ${this._table} (${fields}) VALUES (${valuesPlaceholder})`,
-        values
-      );
-
-    return Object.assign({}, data, { [`id_${this._table}`]: meta.insertId });
   }
   async update(id, data) {
     if (!id) throw new Error(`Missing "id" in data for DbStorage.update`);
-
-    const fields = Object.keys(data).join('=?,') + '=?';
-    const values = Object.values(data);
-
-    await pool
-      .promise()
-      .execute(
-        `UPDATE ${this._table} SET ${fields} WHERE id_${this._table} =?`,
-        [...values, id]
-      );
-    return data;
+    try {
+      const fields = Object.keys(data).join('=?,') + '=?';
+      const values = Object.values(data);
+  
+      await pool
+        .promise()
+        .execute(
+          `UPDATE ${this._table} SET ${fields} WHERE id_${this._table} =?`,
+          [...values, id]
+        );
+      return data;
+    } catch (e) {
+      throw new Error(e);      
+    }
   }
   async remove(id) {
     if (!id) throw new Error(`Missing "id" in data for DbStorage.update`);
-
-    await pool.promise().execute(`DELETE FROM ${this._table} WHERE id_${this._table} =?`, [id])
+    try {
+      await pool.promise().execute(`DELETE FROM ${this._table} WHERE id_${this._table} =?`, [id])
+    } catch (e) {
+      throw new Error(e);      
+    }
   }
 }
 
