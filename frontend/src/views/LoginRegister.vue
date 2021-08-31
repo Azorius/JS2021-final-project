0e022332c9dee64ce24658ecbdacacc93ffc5802
Kristaps <azorien@gmail.com>
2021-08-31 14:24:10 +0300

    Add basic layout, labels

--- frontend/src/views/LoginRegister.vue
+++ frontend/src/views/LoginRegister.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
-    <input type="text" />
-    <input type="text" />
-    <input type="text" />
+  <div class="view">
+    <label>email</label>
+    <input type="text" v-model="email" />
+    <label>username</label>
+    <input type="text" v-model="username" />
+    <label>password</label>
+    <input type="text" v-model="password" />
+    <button @click="login">Login</button>
+    <button @click="register">Register</button>
   </div>
 </template>
 <script>
@@ -15,5 +20,16 @@ export default {
       password: '',
     }
   },
+  methods: {
+    register() {},
+    login() {},
+  },
 }
 </script>
+
+<style scoped>
+.view {
+  display: flex;
+  flex-direction: column;
+}
+</style>
