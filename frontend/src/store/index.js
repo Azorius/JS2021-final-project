68c5afa36c3ead4fdd9b60e906c041813933c6a2
Kristaps <azorien@gmail.com>
2021-08-31 15:05:28 +0300

    Add basic register functionality

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -46,5 +46,24 @@ export default createStore({
           console.log(error)
         })
     },
+    register(context, { email, name, password, callback }) {
+      // todo: username >= 2 characters
+      // todo: passowrd >= 6 characters
+      // todo: validate email
+      console.log(context)
+      axios({
+        method: 'post',
+        url: 'http://localhost:3000/api/users/signup',
+        data: { email, name, password },
+      })
+        .then(response => {
+          //   console.log(response)
+          callback({ response })
+        })
+        .catch(error => {
+          //   console.log(error.response.data)
+          callback(error.response.data)
+        })
+    },
   },
 })
--- frontend/src/views/LoginRegister.vue
+++ frontend/src/views/LoginRegister.vue
@@ -2,8 +2,8 @@
   <div class="view">
     <label>email</label>
     <input type="text" v-model="email" />
-    <label>username</label>
-    <input type="text" v-model="username" />
+    <label>name</label>
+    <input type="text" v-model="name" />
     <label>password</label>
     <input type="text" v-model="password" />
     <button @click="login">Login</button>
@@ -16,12 +16,21 @@ export default {
   data() {
     return {
       email: '',
-      username: '',
+      name: '',
       password: '',
     }
   },
   methods: {
-    register() {},
+    register() {
+      this.$store.dispatch('register', {
+        email: this.email,
+        name: this.name,
+        password: this.password,
+        callback: response => {
+          console.log(response)
+        },
+      })
+    },
     login() {},
   },
 }
