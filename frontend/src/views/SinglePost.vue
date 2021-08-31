c49b8f41bd84044ba6e8b7fd46b593ae6ba75fcf
Kristaps <azorien@gmail.com>
2021-09-01 01:20:06 +0300

    Add additional content to single article page

--- frontend/src/views/SinglePost.vue
+++ frontend/src/views/SinglePost.vue
@@ -6,9 +6,11 @@
         backgroundImage: `url(http://localhost:3000/pictures/${article.imgName})`,
       }"
     >
-      <h2>{{ article.title }}</h2>
+      <h1>{{ article.title }}</h1>
     </div>
-    <p>{{ article.description }}</p>
+    <p>By: {{ article.owner.name }} Posted: {{ article.date }}</p>
+    <br />
+    <h4>{{ article.description }}</h4>
     <p>{{ article.text }}</p>
   </div>
 </template>
@@ -20,10 +22,12 @@ export default {
       article: {},
     }
   },
-  mounted() {
+  created() {
+    console.log('mounted')
     this.article = this.$store.getters.articles.find(
       article => article.id == this.$route.params.id
     )
+    console.log(this.article)
   },
 }
 </script>
@@ -33,13 +37,24 @@ export default {
   display: flex;
   flex-direction: column;
   width: 100%;
+  margin: var(--spacing);
 }
 
 .article-image {
   width: 100%;
+  height: 19rem;
 
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center center;
+
+  display: flex;
+}
+
+.article-image > h1 {
+  align-self: flex-end;
+  color: #fff;
+  padding-bottom: var(--spacing);
+  padding-left: var(--spacing);
 }
 </style>
