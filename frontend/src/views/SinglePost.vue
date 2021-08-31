73bf60a16a491ecc2bce7fff0b1235086bc35fff
Kristaps <azorien@gmail.com>
2021-08-31 23:45:14 +0300

    Add basic structure to SinglePost view

--- frontend/src/views/SinglePost.vue
+++ frontend/src/views/SinglePost.vue
@@ -1,6 +1,21 @@
-<template> </template>
+<template>
+  <div class="view">
+    <div class="image-header">
+      <h2>Title</h2>
+    </div>
+    <p>Description</p>
+    <p>Text</p>
+  </div>
+</template>
 <script>
 export default {
   name: 'SinglePost',
 }
 </script>
+
+<style scoped>
+.view {
+  display: flex;
+  flex-direction: column;
+}
+</style>
