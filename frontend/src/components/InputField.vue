daedfd526dae09ab46919edf53e314e7c5059956
Kristaps <azorien@gmail.com>
2021-09-01 20:47:30 +0300

    Add input field component

---
+++ frontend/src/components/InputField.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input :type="type" :minlength="minlength" :maxlength="maxlength" />
+  </div>
+</template>
+<script>
+export default {
+  name: 'InputField',
+  props: {
+    label: {
+      default: '',
+      type: String,
+    },
+    type: {
+      default: 'text',
+      type: String,
+    },
+    minlength: {
+      default: 0,
+      type: Number,
+    },
+    maxlength: {
+      default: null,
+      type: Number,
+    },
+  },
+}
+</script>
